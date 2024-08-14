7e67b340028509b437e13b6f551ea57e98676f35
stoachup <christophe.druet@entsoe.eu>
2024-08-14 06:20:00 +0300

    Adjustments to pyproject.toml and to metadata in .py

--- ez_config_mgt/__init__.py
+++ ez_config_mgt/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 __author__ = 'Christophe Druet'
-__copyright__ = "Copyright (c) 2024 ENTSO-E AISBL - All rights reserved."
+__copyright__ = "Copyright (c) 2024-, Stoachup SRL - All rights reserved."
 __credits__ = ['Christophe Druet']
 __license__ = 'MIT'
 __version__ = '0.1.1'
 __maintainer__ = 'Christophe Druet'
-__email__ = 'christophe.druet@entsoe.eu'
-__status__ = 'Dev'
+__email__ = 'christophe@stoachup.com'
+__status__ = 'Production'
 
 from .core import BaseConfig
 from .core import extend_default_config
--- ez_config_mgt/core.py
+++ ez_config_mgt/core.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 __author__ = 'Christophe Druet'
-__copyright__ = 'Copyright (c) 2016-, Stoachup SRL - All rights reserved'
+__copyright__ = 'Copyright (c) 2024-, Stoachup SRL - All rights reserved'
 __credits__ = ['Christophe Druet']
-__license__ = 'Proprietary'
+__license__ = 'MIT'
 __version__ = '0.1.1'
 __maintainer__ = 'Christophe Druet'
 __email__ = 'christophe.druet@stoachup.com'
